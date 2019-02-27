c1a8c9a15978b3ac32e68c881a9ead1c9bf3cc11
Colin DeCarlo <colin@thedecarlos.ca>
2019-02-27 00:16:58 -0500

    Extract user card to partial

---
+++ templates/cards/user.php
@@ -0,0 +1,21 @@
+<div class="card mb-4">
+    <div class="card-body">
+        <h5 class="card-title"><?= $this->e($user['name'] ?? $user['email']) ?></h5>
+        <dl class="row">
+            <dt class="col-sm-3">Email</dt>
+            <dd class="col-sm-9"><?= $this->e($user['email']) ?></dd>
+
+            <dt class="col-sm-3">Name</dt>
+            <dd class="col-sm-9"><?= $this->e($user['name']) ?></dd>
+
+            <dt class="col-sm-3">Username</dt>
+            <dd class="col-sm-9"><?= $this->e($user['username']) ?></dd>
+
+            <dt class="col-sm-3">Verified</dt>
+            <dd class="col-sm-9"><?= $user['is_verified'] ? 'Yes' : 'No' ?></dd>
+
+            <dt class="col-sm-3">Created</dt>
+            <dd class="col-sm-9"><?= $this->e($user['created_at']) ?></dd>
+        </dl>
+    </div>
+</div>
